2709a1be14a9d59694ae950106ccbadb0c6b0ffa
Fenja Kollasch <kollasch@uni-bremen.de>
2019-01-18 14:07:28 +0100

    Include Arthurs Perception MSGs

--- src/PositionMsg.cpp
+++ src/PositionMsg.cpp
@@ -6,7 +6,12 @@
 #include <rs/scene_cas.h>
 #include <rs/utils/time.h>
 
+#include <suturo_perception_msgs/ObjectDetectionData.h>
+#include <geometry_msgs/PoseStamped.h>
+
 using namespace uima;
+using namespace suturo_perception_msgs;
+using namespace geometry_msgs;
 
 class PositionMsg : public Annotator
 {
@@ -36,10 +41,53 @@ public:
     std::vector<rs::Cluster> clusters;
     scene.identifiables.filter(clusters);
     for(auto &cluster : clusters) {
-      std::ostringstream output;
+      auto shape = get_shape(cluster);
+      if(!shape.empty())  {
+          std::vector<rs::PoseAnnotation> poses;
+          cluster.annotations.filter(poses);
+          for(auto &pose : poses) {
+              ObjectDetectionData odd = ObjectDetectionData();
+              odd.name = "Object";
+              odd.shape = shape_map(shape);
+              PoseStamped poseStamped = PoseStamped();
+              rsPoseToGeoPose(pose.world.get(), poseStamped);
+              odd.pose = poseStamped;
+          }
+      }
     }
     return UIMA_ERR_NONE;
   }
+
+    std::string get_shape(rs::Cluster cluster) {
+        std::vector<rs::Shape> shapeAnnots;
+        cluster.annotations.filter(shapeAnnots);
+        if(!shapeAnnots.empty())
+            return shapeAnnots[0].shape.get();
+        return "";
+    }
+
+    u_int shape_map(const std::string shape) {
+      if(shape == "round" || shape == "cylinder") {
+          return ObjectDetectionData::CYLINDER;
+      } else if( shape == "box") {
+          return  ObjectDetectionData::BOX;
+      }
+      return ObjectDetectionData::MISC;
+    }
+
+    void rsPoseToGeoPose(rs::StampedPose pose, PoseStamped geoPose) {
+        auto translation = pose.translation.get();
+        auto rotation = pose.rotation.get();
+        geoPose.pose.position.x = translation[0];
+        geoPose.pose.position.y = translation[1];
+        geoPose.pose.position.z = translation[2];
+        geoPose.pose.orientation.x = rotation[0];
+        geoPose.pose.orientation.y = rotation[1];
+        geoPose.pose.orientation.z = rotation[2];
+        geoPose.pose.orientation.w = rotation[3];
+    }
+
+
 };
 
 // This macro exports an entry point that is used to create the annotator.
