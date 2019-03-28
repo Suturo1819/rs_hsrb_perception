ff579e21fde58834bf22fa13badcd709575d5124
Fenja Kollasch <kollasch@uni-bremen.de>
2019-03-28 16:53:32 +0100

    Cmake pls

--- src/SuturoProcessManager.cpp
+++ src/SuturoProcessManager.cpp
@@ -3,19 +3,15 @@
  * Optimized for the Suturo Perception Pipelines
  * @author Fenja Kollasch
  */
-#include "../include/rs_hsrb_perception/SuturoProcessManager.h"
+#include <rs_hsrb_perception/SuturoProcessManager.h>
 
 SuturoProcessManager::SuturoProcessManager(ros::NodeHandle n, const std::string savePath)
-:   RSProcessManager(false, false, RSProcessManager::KnowledgeEngineType::SWI_PROLOG, n, savePath),
+:   RSProcessManager(true, false, RSProcessManager::KnowledgeEngineType::SWI_PROLOG, n, savePath),
     savePath(savePath)
 {
     outInfo("A RoboSherlock process manager optimized for the Suturo perception was created.");
 }
 
-SuturoProcessManager::~SuturoProcessManager() {
-    uima::ResourceManager::deleteInstance();
-    outInfo("Suturo Process Manager stopped.");
-}
 
 
 void SuturoProcessManager::init(std::string &xmlFile) {
@@ -23,10 +19,8 @@ void SuturoProcessManager::init(std::string &xmlFile) {
 }
 
 void SuturoProcessManager::run(bool visualize) {
-    if(visualize) {
+    if(!visualize) {
         visualizer_.stop();
-        suturo_vis = new rs::Visualizer(savePath, false);
-        suturo_vis->start();
     }
     signal(SIGINT, RSProcessManager::signalHandler);
     {
